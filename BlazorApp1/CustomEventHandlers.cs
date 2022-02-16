2b5a187597ddd3a3321f7d41253907cf3c8853a6
Aleksey Zlotnikov <azlotnikov@lightpointglobal.com>
2022-02-16 13:00:38 +0300

    init

---
+++ BlazorApp1/CustomEventHandlers.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1;
+
+[EventHandler("onpastemultimedia", typeof(PasteMultimediaEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+public static class CustomEventHandlers
+{
+	
+}
+
+public class PasteMultimediaEventArgs : EventArgs
+{
+	//public IEnumerable<ValueTuple<bool, string>> PastedMultimedia { get; set; }
+	public bool IsMultimedia { get; set; }
+	public string Data { get; set; }
+}
---
+++ BlazorApp1/wwwroot/js/customEvents.js
@@ -0,0 +1,42 @@
+﻿Blazor.registerCustomEventType('pastemultimedia', {
+	browserEventName: 'paste',
+	createEventArgs: event => {
+		let isMultimedia = false;
+
+		// We get the saved text in the clipboard
+		let data = event.clipboardData.getData('text');
+
+		const items = event.clipboardData.items;
+
+		// With this we'll filter the file by its media type
+		const acceptedMediaTypes = ['image/png', 'image/jpeg', 'image/bmp'];
+
+		//const dataList = [{ isMultimedia, data }];
+
+		for (let i = 0; i < items.length; i++) {
+			const file = items[i].getAsFile();
+
+			isMultimedia = false;
+
+			// We verify there's a file in the current item
+			if (!file) {
+				continue;
+			}
+
+			// We verify the media type of the file
+			if (acceptedMediaTypes.indexOf(items[i].type) === -1) {
+				continue;
+			}
+
+			// it's an image
+			isMultimedia = true;
+			const url = window.URL || window.webkitURL;
+			data = url.createObjectURL(file);
+
+			//dataList.push({ isMultimedia, data });
+		}
+
+		//return dataList;
+		return {isMultimedia, data};
+	}
+});
